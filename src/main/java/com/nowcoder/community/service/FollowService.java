a1821774f057b0b55aac404ebcc4642ddec21c66
cjx <cjxnihao@gmail.com>
2022-06-28 00:28:01 +0800

    add "follower-list" and "followee-list" functions

--- src/main/java/com/nowcoder/community/service/FollowService.java
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -1,5 +1,7 @@
 package com.nowcoder.community.service;
 
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -7,6 +9,8 @@ import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 /**
  * @ClassName FollowService
@@ -16,28 +20,33 @@ import org.springframework.stereotype.Service;
  * @Version 1.0
  */
 @Service
-public class FollowService {
+public class FollowService implements CommunityConstant {
 
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private UserService userService;
+
+
     /**
      * 关注功能
-     * @param userId 关注者id
+     *
+     * @param userId     关注者id
      * @param entityType 被关注实体类型
-     * @param entityId 被关注实体id
+     * @param entityId   被关注实体id
      */
-    public void follow(int userId,int entityType,int entityId){
+    public void follow(int userId, int entityType, int entityId) {
         redisTemplate.execute(new SessionCallback<>() {
             @Override
-            public  Object execute(RedisOperations operations) throws DataAccessException {
+            public Object execute(RedisOperations operations) throws DataAccessException {
                 String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
                 String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
 
                 operations.multi();
 
-                operations.opsForZSet().add(followeeKey,entityId,System.currentTimeMillis());
-                operations.opsForZSet().add(followerKey,userId,System.currentTimeMillis());
+                operations.opsForZSet().add(followeeKey, entityId, System.currentTimeMillis());
+                operations.opsForZSet().add(followerKey, userId, System.currentTimeMillis());
 
                 return operations.exec();
             }
@@ -46,21 +55,22 @@ public class FollowService {
 
     /**
      * 取消关注功能
+     *
      * @param userId
      * @param entityType
      * @param entityId
      */
-    public void unfollow(int userId,int entityType,int entityId){
+    public void unfollow(int userId, int entityType, int entityId) {
         redisTemplate.execute(new SessionCallback<>() {
             @Override
-            public  Object execute(RedisOperations operations) throws DataAccessException {
+            public Object execute(RedisOperations operations) throws DataAccessException {
                 String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
                 String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
 
                 operations.multi();
 
-                operations.opsForZSet().remove(followeeKey,entityId);
-                operations.opsForZSet().remove(followerKey,userId);
+                operations.opsForZSet().remove(followeeKey, entityId);
+                operations.opsForZSet().remove(followerKey, userId);
 
                 return operations.exec();
             }
@@ -69,35 +79,85 @@ public class FollowService {
 
     /**
      * 查询关注的实体的数量
+     *
      * @param userId
      * @param entityType
      * @return
      */
-    public long findFolloweeCount(int userId,int entityType){
-        String followeeKey=RedisKeyUtil.getFolloweeKey(userId,entityType);
+    public long findFolloweeCount(int userId, int entityType) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().zCard(followeeKey);
     }
 
     /**
      * 查询实体的粉丝的数量
+     *
      * @param entityType
      * @param entityId
      * @return
      */
-    public long findFollowerCount(int entityType,int entityId){
-        String followerKey=RedisKeyUtil.getFollowerKey(entityType,entityId);
+    public long findFollowerCount(int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
         return redisTemplate.opsForZSet().zCard(followerKey);
     }
 
     /**
      * 查询当前用户是否已关注该实体
+     *
      * @param userId
      * @param entityType
      * @param entityId
      * @return
      */
-    public boolean hasFollowed(int userId,int entityType,int entityId){
-        String followeeKey=RedisKeyUtil.getFolloweeKey(userId,entityType);
-        return redisTemplate.opsForZSet().score(followeeKey,entityId)!=null;
+    public boolean hasFollowed(int userId, int entityType, int entityId) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+    /**
+     * 查询某用户关注的人
+     *
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowees(int userId, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followeeKey, offset, offset + limit - 1);
+        if (set == null) return null;
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer id : set) {
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(id);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followeeKey, id);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
+    }
+
+    /**
+     * 查询某用户的粉丝
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowers(int userId, int offset, int limit) {
+        String followerKey = RedisKeyUtil.getFollowerKey( ENTITY_TYPE_USER,userId);
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset + limit - 1);
+        if (set == null) return null;
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer id : set) {
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(id);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followerKey, id);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
     }
 }
