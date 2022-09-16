a0bdc9c09fb016b0d0d75ac6522af20e362f0c7e
cjx <cjxnihao@gmail.com>
2022-09-17 02:08:09 +0800

    add "show system message detail" function

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -17,33 +17,38 @@ import java.util.List;
 public interface MessageMapper {
 
     //查询当前用户的会话列表,针对每个会话只返回一条最新的私信
-    List<Message> selectConversations(int userId,int offset,int limit);
+    List<Message> selectConversations(int userId, int offset, int limit);
 
     //查询当前用户的会话数量
     int selectConversationCount(int userId);
 
     //查询某个会话所包含的私信列表
-    List<Message> selectLetters(String conversationId,int offset,int limit);
+    List<Message> selectLetters(String conversationId, int offset, int limit);
 
     //查询某个会话所包含的私信数量
     int selectLetterCount(String conversationId);
 
     //查询未读私信的数量
-    int selectLetterUnreadCount(int userId,@Nullable String conversationId);
+    int selectLetterUnreadCount(int userId, @Nullable String conversationId);
 
     //新增消息
     int insertMessage(Message message);
 
     //修改消息的状态
-    int updateStatus(List<Integer> ids,int status);
+    int updateStatus(List<Integer> ids, int status);
 
     //删除某条信息
     int deleteMessage(int id);
 
     //查询某个主题下最新的通知
-    Message selectLatestNotice(int userId,String topic);
+    Message selectLatestNotice(int userId, String topic);
+
     //查询某个主题所包含的通知数量
-    int selectNoticeCount(int userId,String topic);
+    int selectNoticeCount(int userId, String topic);
+
     //查询未读的通知的数量
-    int selectNoticeUnreadCount(int userId,String topic);
+    int selectNoticeUnreadCount(int userId, String topic);
+
+    //查询某一个主题所包含的所有通知列表
+    List<Message> selectNotices(int userId,String topic,int offset,int limit);
 }
